940c13a81b34b596337cd8f0c10e02260ca28700
jsoulier <jaansoulier@gmail.com>
2025-04-11 02:14:59 -0400

    setup

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
+{
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+    return SDL_APP_SUCCESS;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+    return SDL_APP_SUCCESS;
+}
